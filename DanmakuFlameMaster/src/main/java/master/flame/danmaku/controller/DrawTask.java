138a0e059f17c0846b0a614b9ae61810f2f66662
calmer <calmer91@gmail.com>
2014-04-13 23:14:48 +0800

    IDrawTask: add requestClear method

--- DanmakuFlameMaster/src/main/java/master/flame/danmaku/controller/DrawTask.java
+++ DanmakuFlameMaster/src/main/java/master/flame/danmaku/controller/DrawTask.java
@@ -105,7 +105,7 @@ public class DrawTask implements IDrawTask {
     @Override
     public void seek(long mills) {
         reset();
-        requestCanvasClear();
+        requestClear();
         GlobalFlagValues.updateVisibleFlag();
         mStartRenderTime = mills < 1000 ? 0 : mills;
     }
@@ -160,7 +160,7 @@ public class DrawTask implements IDrawTask {
         }
     }
     
-    protected void requestCanvasClear(){
+    public void requestClear(){
         clearFlag = 5;
     }
 
